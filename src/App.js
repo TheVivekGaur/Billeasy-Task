0e602a3da5a391d7505874c245c6744ee1f7c147
TheVivekGaur <vvksngh24@gmail.com>
2021-10-14 00:05:34 +0530

    Expand of image onCick

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
   import { useState,useEffect } from 'react';
   import axios from 'axios';
-  import Images from './Images';
-  import Modal from './Modal';
+  import Images from './Components/Images/Images';
+  import Modal from './Components/Modal/Modal';
 
   function App() {
     const [imageList, setImageList] = useState([]);
@@ -14,7 +14,6 @@
     const  fetchImageList = async () =>{
           try{  
         const response = await axios.get(' http://www.mocky.io/v2/5ecb5c353000008f00ddd5a0')
-          // console.log(response.data);
           const imageData = await response.data;
           setImageList(imageData)
           }catch(err){
--- src/Components/Images/Images.jsx
+++ src/Components/Images/Images.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 import './Images.css'
 
 const Images= ({items,  setSelectedImg}) => {
- // console.log(items)
  return(
   <div className = "img-grid">
  {
     items && items.map( item => (
      <div className="img-wrap" key={item.id}
-          onClick={()=> setSelectedImg(item.urls.thumb)}
+          onClick={()=> setSelectedImg(item.urls.regular)}
      >
       <img src={item.urls.thumb} alt="fetchedimage" />
       </div>
